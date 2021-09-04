e5964e089008e78eb35698b844f6dca5cce53ceb
unknown <idea7654@naver.com>
2021-09-04 20:19:23 +0900

    start to convert unity pj to unreal

---
+++ Source/BattleLoyal/BattleLoyal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BattleLoyal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleLoyal, "BattleLoyal" );
